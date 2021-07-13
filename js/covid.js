ab7ae683046909b7c9367c0ce41db4f53bb7e4d7
Ganning Xu <ganningxu@gmail.com>
2021-07-13 16:17:29 -0400

    animations

--- js/covid.js
+++ js/covid.js
@@ -3,15 +3,32 @@ function init() {
     fetch(endpoint)
         .then(resp => resp.json())
         .then(data => {
-            const globalCases = addCommas(data.Global.TotalConfirmed) + " cases.";
-            const globalDeaths = addCommas(data.Global.TotalDeaths) + " deaths."
-
-            document.getElementById("covid-title").innerHTML = `${globalCases}<br>${globalDeaths}` ;
+            animateValue("cases", data.Global.TotalConfirmed - 1000, data.Global.TotalConfirmed, 3000, "deaths");
+            animateValue("deaths", data.Global.TotalDeaths - 1000, data.Global.TotalDeaths, 3000, "cases");
+        
         })
 }
 
 function addCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+function animateValue(id, start, end, duration, unit) {
+    if (start === end) return;
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = `${addCommas(current)} ${unit}`;
+
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
 
 init();
